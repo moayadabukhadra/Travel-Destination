d456f8ec05a81c4938852a54df806f486e32f055
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-02-14 00:00:07 +0200

    class16

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+
+
+function Footer(){
+return <div>
+    
+    <a href="https://github.com/moayadabukhadra" >github</a>
+
+<a href="https://web.facebook.com/moayad.abukhadra.79/" >facebook</a>
+
+<h3>Moayad abukhadra</h3>
+
+</div>
+}
+export default Footer;
